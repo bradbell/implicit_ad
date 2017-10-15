fd5273112cad678fef05a0bdc252a5ab2f2645a0
Brad Bell <bradbell@seanet.com>
2017-10-15 05:05:01 -0700

    Add verbose and build_type flags to cmake comamnd.

--- bin/run_cmake.sh
+++ bin/run_cmake.sh
@@ -1,5 +1,10 @@
 #! /bin/bash -e
 # -----------------------------------------------------------------------------
+# value used for CMAKE_VERBOSE_MAKEFILE
+cmake_verbose_makefile='false'
+# value used for CMAKE_BULD_TYPE
+cmake_build_type='release'
+# extra flags, besides debug and release flags, used during compliation
 extra_cxx_flags='-Wall -pedantic-errors -std=c++11 -Wshadow'
 # -----------------------------------------------------------------------------
 if [ "$0" != 'bin/run_cmake.sh' ]
@@ -12,4 +17,9 @@ then
 	mkdir build
 fi
 cd build
-cmake -D extra_cxx_flags="${extra_cxx_flags}" ..
+source_dir='..'
+cmake \
+	-D CMAKE_VERBOSE_MAKEFILE="${cmake_verbose_makefile}" \
+	-D CMAKE_BUILD_TYPE="${cmake_build_type}" \
+	-D extra_cxx_flags="${extra_cxx_flags}" \
+	${source_dir}
