a1ca41759cb9fa55af74838bfa69134873cd63cb
Brad Bell <bradbell@seanet.com>
2017-10-15 06:06:18 -0700

    run_cmake.sh: doc pkg-config search path.

--- bin/run_cmake.sh
+++ bin/run_cmake.sh
@@ -11,6 +11,10 @@
 #	fi
 #	mkdir
 #	dir
+#	cppad
+#	config
+#	pkgconfig
+#	eigen
 # &&
 #
 # &section Run CMake to Configure Implicit AD&&
@@ -19,9 +23,34 @@
 # &codei%bin/run_cmake.sh%&&
 #
 # &head Purpose&&
-# This is a bash shell script that configures &code implicit_ad&&
-# by running CMake. If you do not have bash on your system,
+# This is a
+# &href%https://en.wikipedia.org/wiki/Bash_(Unix_shell)%bash%&&
+# script that configures &code implicit_ad&&
+# by running &href%https://en.wikipedia.org/wiki/CMake%CMake%&&.
+# If you do not have bash on your system,
 # this script can be considered documentation for the &code cmake&& command.
+#
+# &head cppad_pkg_config_path&&
+# This is the directory where the file &code cppad.pc&& file is located
+# &srccode%sh%
+cppad_pkg_config_path="$HOME/prefix/cppad/share/pkgconfig"
+# %&&
+#
+# &head eigen_pkg_config_path&&
+# This is the directory where the file &code eigen3.pc&& file is located.
+# It is often desirable to install eigen its own special prefix
+# so warnings can be suppressed for its include files without suppressing
+# warnings for other include files.
+# &srccode%sh%
+eigen_pkg_config_path="$HOME/prefix/eigen/share/pkgconfig"
+# %&&
+#
+# &head PKG_CONFIG_PATH&&
+# Set the directories that are searched by
+# &href%https://en.wikipedia.org/wiki/Pkg-config%pkg-config%&&.
+# &srccode%sh%
+export PKG_CONFIG_PATH="${cppad_pkg_config_path}:${eigen_pkg_config_path}"
+# %&&
 #
 # &head cmake_verbose_makefile&&
 # If this is &code true&&, make will display the compile and link commands
