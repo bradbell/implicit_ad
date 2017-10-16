4f4ee1ac06b98bc02d96e9bc2664b54eab79bb5e
Brad Bell <bradbell@seanet.com>
2017-10-15 18:19:21 -0700

    version.sh: maintains version number.

---
+++ bin/version.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+if [ "$0" != 'bin/version.sh' ]
+then
+	echo 'bin/version.sh: must be run from its parent directory'
+	exit 1
+fi
+if [ "$1" == 'get' ]
+then
+	sed -n -e '/SET( *implicit_ad_version/p' CMakeLists.txt |
+		sed -e 's/SET( *implicit_ad_version *"//' -e 's/" *)//'
+	exit 0
+fi
+if [ "$1" == 'set' ]
+then
+	version=`date +%Y%m%d`
+	sed -i CMakeLists.txt \
+		-e "s|\(SET( *implicit_ad_version\)* \"[0-9]\{8\}\"|\1 \"$version\")|"
+	sed -i implicit_ad.omh \
+		-e "s|implicit_ad-[0-9]\\{8\\}|implicit_ad-$version|"
+fi
