df561243dd8304c7198e276d860276750af3de1d
Brad Bell <bradbell@seanet.com>
2017-10-19 06:03:19 -0700

    Advance to implicid_at-20171019. version.sh: add usages and exit messages.

--- bin/version.sh
+++ bin/version.sh
@@ -10,16 +10,21 @@ then
 	sed -n -e '/SET( *implicit_ad_version/p' CMakeLists.txt |
 		sed -e 's/SET( *implicit_ad_version *"//' -e 's/" *)//'
 	exit 0
-fi
-if [ "$1" == 'set' ]
+elif [ "$1" == 'set' ]
 then
 	version=`date +%Y%m%d`
 	sed -i CMakeLists.txt \
 		-e "s|\(SET( *implicit_ad_version\)* \"[0-9]\{8\}\"|\1 \"$version\"|"
-fi
-if [ "$1" == 'copy' ]
+	echo 'bin/version.sh: OK'
+	exit 0
+elif [ "$1" == 'copy' ]
 then
 	version=`bin/version.sh get`
 	sed -i implicit_ad.omh \
 		-e "s|implicit_ad-[0-9]\\{8\\}|implicit_ad-$version|"
+	echo 'bin/version.sh: OK'
+	exit 0
+else
+	echo 'usage: bin/version.sh (get|set|copy)'
+	exit 1
 fi
